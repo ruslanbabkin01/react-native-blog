3f662c2fce3905fc3dc8a5b54a1824ef9a96eca6
Ruslan Babkin <ruslan.babkin01@gmail.com>
2022-12-29 18:24:08 +0200

    add fonts

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React, { useState, useEffect } from 'react';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
 import {
   ImageBackground,
   StyleSheet,
@@ -6,92 +9,175 @@ import {
   View,
   TouchableOpacity,
   Platform,
-} from "react-native";
+  KeyboardAvoidingView,
+  Keyboard,
+  TouchableWithoutFeedback,
+  Dimensions,
+} from 'react-native';
+
+const initialState = {
+  email: '',
+  password: '',
+};
+
+const loadFonts = async () => {
+  await Font.loadAsync({
+    'Roboto-Regulat': require('./assets/fonts/Roboto-Regular.ttf'),
+    'Roboto-Bold': require('./assets/fonts/Roboto-Bold.ttf'),
+    'Roboto-Medium': require('./assets/fonts/Roboto-Medium.ttf'),
+    'SFP-Light': require('./assets/fonts/SFProDisplay-Light.ttf'),
+    'SFP-Regular': require('./assets/fonts/SFProDisplay-Regular.ttf'),
+  });
+};
 
 export default function App() {
+  const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+  const [state, setState] = useState(initialState);
+  const [isReady, setIsReady] = useState(false);
+  const [dimensions, setDimensions] = useState(
+    Dimensions.get('window').width - 20 * 2
+  );
+
+  if (!isReady) {
+    return (
+      <AppLoading startAsync={loadFonts} onFinish={() => setIsReady(true)} />
+    );
+  }
+
+  function keyboardHide() {
+    setIsShowKeyboard(false);
+    Keyboard.dismiss();
+    setState(initialState);
+  }
+
+  useEffect(() => {
+    const onChange = () => {
+      const width = Dimensions.get('window').width - 20 * 2;
+    };
+    Dimensions.addEventListener('change', onChange);
+    return () => {
+      Dimensions.removeEventListener('change', onChange);
+    };
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <ImageBackground
-        style={styles.image}
-        source={require("./assets/images/stars.jpg")}>
-        <View style={styles.form}>
-          <View>
-            <Text style={styles.inputTitle}>EMAIL ADRESS</Text>
-            <TextInput
-              style={styles.input}
-              placeholder='Please enter'
-              textAlign='center'
-            />
-          </View>
-          <View style={{ marginTop: 20 }}>
-            <Text style={styles.inputTitle}>PASSWORD</Text>
-            <TextInput
-              style={styles.input}
-              placeholder='Please enter'
-              textAlign='center'
-              secureTextEntry={true}
-            />
-          </View>
-          <TouchableOpacity style={styles.btn} activeOpacity={0.8}>
-            <Text style={styles.btnTitle}>SIGN IN</Text>
-          </TouchableOpacity>
-        </View>
-      </ImageBackground>
-    </View>
+    <TouchableWithoutFeedback onPress={keyboardHide}>
+      <View style={styles.container}>
+        <ImageBackground
+          style={styles.image}
+          source={require('./assets/images/stars.jpg')}
+        >
+          <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+          >
+            <View
+              style={{
+                ...styles.form,
+                marginBottom: isShowKeyboard ? 40 : 100,
+                width: dimensions,
+              }}
+            >
+              <View style={styles.header}>
+                <Text style={styles.headerTitle}>Hello again</Text>
+                <Text style={styles.headerTitle}>Welcome Back</Text>
+              </View>
+              <View>
+                <Text style={styles.inputTitle}>EMAIL ADRESS</Text>
+                <TextInput
+                  style={styles.input}
+                  textAlign={'center'}
+                  onFocus={() => setIsShowKeyboard(true)}
+                  value={state.email}
+                  onChangeText={value =>
+                    setState(prevState => ({ ...prevState, email: value }))
+                  }
+                />
+              </View>
+              <View style={{ marginTop: 20 }}>
+                <Text style={styles.inputTitle}>PASSWORD</Text>
+                <TextInput
+                  style={styles.input}
+                  textAlign={'center'}
+                  secureTextEntry={true}
+                  onFocus={() => setIsShowKeyboard(true)}
+                  value={state.password}
+                  onChangeText={value =>
+                    setState(prevState => ({ ...prevState, password: value }))
+                  }
+                />
+              </View>
+              <TouchableOpacity
+                style={styles.btn}
+                activeOpacity={0.8}
+                onPress={keyboardHide}
+              >
+                <Text style={styles.btnTitle}>SIGN IN</Text>
+              </TouchableOpacity>
+            </View>
+          </KeyboardAvoidingView>
+        </ImageBackground>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "center",
-  },
-  inputTitle: {
-    marginBottom: 10,
-    color: "#fff",
-    fontSize: 18,
+    backgroundColor: '#fff',
   },
   image: {
     flex: 1,
-    resizeMode: "cover",
+    resizeMode: 'cover',
+    justifyContent: 'flex-end',
     // justifyContent: "center",
-    justifyContent: "flex-end",
-    // alignItems: "center",
+    alignItems: 'center',
   },
   input: {
     borderWidth: 1,
-    borderColor: "#faebd7",
+    borderColor: '#faebd7',
     borderRadius: 6,
     height: 40,
-    color: "#faebd7",
+    color: '#faebd7',
+  },
+  inputTitle: {
+    marginBottom: 10,
+    color: '#fff',
+    fontSize: 18,
   },
   form: {
-    marginHorizontal: 40,
+    // marginHorizontal: 40,
   },
   btn: {
     height: 40,
     borderRadius: 6,
     borderWidth: 1,
     marginTop: 40,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     marginHorizontal: 20,
     ...Platform.select({
       ios: {
-        backgroundColor: "transparent",
-        borderColor: "#faebd7",
+        backgroundColor: 'transparent',
+        borderColor: '#faebd7',
       },
       android: {
-        backgroundColor: "#4169e1",
-        borderColor: "transparent",
+        backgroundColor: '#4169e1',
+        borderColor: 'transparent',
       },
     }),
-    // backgroundColor: Platform.OS === "ios" ? "transparent" : "#4169e1",
-    // borderColor: Platform.OS === "ios" ? "#faebd7" : "transparent",
   },
   btnTitle: {
-    color: Platform.OS === "ios" ? "#4169e1" : "#fff",
+    color: Platform.OS === 'ios' ? '#4169e1' : '#faebd7',
     fontSize: 18,
+  },
+  header: {
+    alignItems: 'center',
+    marginBottom: 120,
+  },
+  headerTitle: {
+    // fontFamily: 'SFP-Regular',
+    fontSize: 30,
+    color: '#faebd7',
   },
 });
