58c0945b108794ad497aa9fd696d732c20593e03
Ruslan Babkin <ruslan.babkin01@gmail.com>
2023-01-02 11:17:07 +0200

    add components

--- App.js
+++ App.js
@@ -1,95 +1,97 @@
-import React, { useState, useEffect } from 'react';
-import * as Font from 'expo-font';
-import { AppLoading } from 'expo';
+import React, { useState, useEffect } from "react";
 import {
-  ImageBackground,
   StyleSheet,
   Text,
-  TextInput,
   View,
+  ImageBackground,
+  TextInput,
   TouchableOpacity,
   Platform,
   KeyboardAvoidingView,
   Keyboard,
   TouchableWithoutFeedback,
   Dimensions,
-} from 'react-native';
+} from "react-native";
+
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
 
 const initialState = {
-  email: '',
-  password: '',
+  email: "",
+  password: "",
 };
 
-const loadFonts = async () => {
+const loadApplication = async () => {
   await Font.loadAsync({
-    'Roboto-Regulat': require('./assets/fonts/Roboto-Regular.ttf'),
-    'Roboto-Bold': require('./assets/fonts/Roboto-Bold.ttf'),
-    'Roboto-Medium': require('./assets/fonts/Roboto-Medium.ttf'),
-    'SFP-Light': require('./assets/fonts/SFProDisplay-Light.ttf'),
-    'SFP-Regular': require('./assets/fonts/SFProDisplay-Regular.ttf'),
+    "SFP-Regular": require("./assets/fonts/SFProDisplay-Regular.ttf"),
   });
 };
 
 export default function App() {
+  console.log(Platform.OS);
   const [isShowKeyboard, setIsShowKeyboard] = useState(false);
-  const [state, setState] = useState(initialState);
-  const [isReady, setIsReady] = useState(false);
-  const [dimensions, setDimensions] = useState(
-    Dimensions.get('window').width - 20 * 2
+  const [state, setstate] = useState(initialState);
+  const [iasReady, setIasReady] = useState(false);
+
+  const [dimensions, setdimensions] = useState(
+    Dimensions.get("window").width - 20 * 2
   );
-
-  if (!isReady) {
-    return (
-      <AppLoading startAsync={loadFonts} onFinish={() => setIsReady(true)} />
-    );
-  }
-
-  function keyboardHide() {
-    setIsShowKeyboard(false);
-    Keyboard.dismiss();
-    setState(initialState);
-  }
 
   useEffect(() => {
     const onChange = () => {
-      const width = Dimensions.get('window').width - 20 * 2;
+      const width = Dimensions.get("window").width - 20 * 2;
+
+      setdimensions(width);
     };
-    Dimensions.addEventListener('change', onChange);
+    Dimensions.addEventListener("change", onChange);
     return () => {
-      Dimensions.removeEventListener('change', onChange);
+      Dimensions.removeEventListener("change", onChange);
     };
   }, []);
+
+  const keyboardHide = () => {
+    setIsShowKeyboard(false);
+    Keyboard.dismiss();
+    setstate(initialState);
+  };
+
+  if (!iasReady) {
+    return (
+      <AppLoading
+        startAsync={loadApplication}
+        onFinish={() => setIasReady(true)}
+        onError={console.warn}
+      />
+    );
+  }
 
   return (
     <TouchableWithoutFeedback onPress={keyboardHide}>
       <View style={styles.container}>
         <ImageBackground
           style={styles.image}
-          source={require('./assets/images/stars.jpg')}
-        >
+          source={require("./assets/images/stars.jpg")}>
           <KeyboardAvoidingView
-            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-          >
+            behavior={Platform.OS == "ios" ? "padding" : "height"}>
             <View
               style={{
                 ...styles.form,
-                marginBottom: isShowKeyboard ? 40 : 100,
+                marginBottom: isShowKeyboard ? 20 : 150,
                 width: dimensions,
-              }}
-            >
+              }}>
               <View style={styles.header}>
                 <Text style={styles.headerTitle}>Hello again</Text>
-                <Text style={styles.headerTitle}>Welcome Back</Text>
+                <Text style={styles.headerTitle}>Welcome back</Text>
               </View>
               <View>
-                <Text style={styles.inputTitle}>EMAIL ADRESS</Text>
+                <Text style={styles.inputTitle}>EMAIL ADDRES</Text>
                 <TextInput
                   style={styles.input}
-                  textAlign={'center'}
+                  textAlign={"center"}
                   onFocus={() => setIsShowKeyboard(true)}
                   value={state.email}
-                  onChangeText={value =>
-                    setState(prevState => ({ ...prevState, email: value }))
+                  onChangeText={(value) =>
+                    setstate((prevState) => ({ ...prevState, email: value }))
                   }
                 />
               </View>
@@ -97,20 +99,19 @@ export default function App() {
                 <Text style={styles.inputTitle}>PASSWORD</Text>
                 <TextInput
                   style={styles.input}
-                  textAlign={'center'}
+                  textAlign={"center"}
                   secureTextEntry={true}
                   onFocus={() => setIsShowKeyboard(true)}
                   value={state.password}
-                  onChangeText={value =>
-                    setState(prevState => ({ ...prevState, password: value }))
+                  onChangeText={(value) =>
+                    setstate((prevState) => ({ ...prevState, password: value }))
                   }
                 />
               </View>
               <TouchableOpacity
-                style={styles.btn}
                 activeOpacity={0.8}
-                onPress={keyboardHide}
-              >
+                style={styles.btn}
+                onPress={keyboardHide}>
                 <Text style={styles.btnTitle}>SIGN IN</Text>
               </TouchableOpacity>
             </View>
@@ -124,60 +125,123 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   image: {
     flex: 1,
-    resizeMode: 'cover',
-    justifyContent: 'flex-end',
-    // justifyContent: "center",
-    alignItems: 'center',
+    resizeMode: "cover",
+    justifyContent: "flex-end",
+    alignItems: "center",
   },
   input: {
     borderWidth: 1,
-    borderColor: '#faebd7',
-    borderRadius: 6,
+    borderColor: "#f0f8ff",
     height: 40,
-    color: '#faebd7',
-  },
-  inputTitle: {
-    marginBottom: 10,
-    color: '#fff',
-    fontSize: 18,
+    borderRadius: 6,
+
+    color: "#f0f8ff",
   },
   form: {
     // marginHorizontal: 40,
   },
+  inputTitle: {
+    color: "#f0f8ff",
+    marginBottom: 10,
+    fontSize: 18,
+    fontFamily: "SFP-Regular",
+  },
   btn: {
-    height: 40,
     borderRadius: 6,
     borderWidth: 1,
+    height: 40,
     marginTop: 40,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     marginHorizontal: 20,
     ...Platform.select({
       ios: {
-        backgroundColor: 'transparent',
-        borderColor: '#faebd7',
+        backgroundColor: "transparent",
+        borderColor: "#f0f8ff",
       },
       android: {
-        backgroundColor: '#4169e1',
-        borderColor: 'transparent',
+        backgroundColor: "#4169e1",
+        borderColor: "transparent",
       },
     }),
   },
   btnTitle: {
-    color: Platform.OS === 'ios' ? '#4169e1' : '#faebd7',
+    color: Platform.OS === "ios" ? "#4169e1" : "#f0f8ff",
     fontSize: 18,
+    fontFamily: "SFP-Regular",
   },
   header: {
-    alignItems: 'center',
+    alignItems: "center",
     marginBottom: 120,
   },
   headerTitle: {
-    // fontFamily: 'SFP-Regular',
-    fontSize: 30,
-    color: '#faebd7',
+    fontSize: 40,
+    color: "#f0f8ff",
+    fontFamily: "SFP-Regular",
   },
 });
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//   },
+//   image: {
+//     flex: 1,
+//     resizeMode: "cover",
+//     justifyContent: "flex-end",
+//     justifyContent: "center",
+//     alignItems: "center",
+//   },
+//   input: {
+//     borderWidth: 1,
+//     borderColor: "#faebd7",
+//     borderRadius: 6,
+//     height: 40,
+//     color: "#faebd7",
+//   },
+//   inputTitle: {
+//     marginBottom: 10,
+//     color: "#fff",
+//     fontSize: 18,
+//   },
+//   form: {
+//     marginHorizontal: 40,
+//   },
+//   btn: {
+//     height: 40,
+//     borderRadius: 6,
+//     borderWidth: 1,
+//     marginTop: 40,
+//     justifyContent: "center",
+//     alignItems: "center",
+//     marginHorizontal: 20,
+//     ...Platform.select({
+//       ios: {
+//         backgroundColor: "transparent",
+//         borderColor: "#faebd7",
+//       },
+//       android: {
+//         backgroundColor: "#4169e1",
+//         borderColor: "transparent",
+//       },
+//     }),
+//   },
+//   btnTitle: {
+//     color: Platform.OS === "ios" ? "#4169e1" : "#faebd7",
+//     fontSize: 18,
+//   },
+//   header: {
+//     alignItems: "center",
+//     marginBottom: 120,
+//   },
+//   headerTitle: {
+//     fontFamily: 'SFP-Regular',
+//     fontSize: 30,
+//     color: "#faebd7",
+//   },
+// });
